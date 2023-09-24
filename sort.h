ef5fe7bf3f9d58464031884459188d87a172e09b
Naouwa <densali.nahla@gmail.com>
2023-09-24 14:07:30 +0100

    betty

--- sort.h
+++ sort.h
@@ -11,19 +11,18 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/**
- * printing functions
- */
+/*printing functions*/
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-/**
- * sorting algorithms prototype
- */
+
+/*sorting algorithms prototype*/
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
